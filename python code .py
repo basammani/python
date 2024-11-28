fe2d98f72d1c43df8d0041de21e1bc7338f93c58
Basammani <83907939+basammani@users.noreply.github.com>
2024-11-28 16:17:49 +0530

    Add files via upload

---
+++ python code .py
@@ -0,0 +1,127 @@
+# TIME= float(input("what is the time nim ? "))
+
+# if TIME >= 1 and TIME ==2:
+#     print("ITS afternoon")
+# elif TIME > 2 and  TIME <=4:
+#     #print("Its evening" )
+#     if TIME<=3:
+#         print("sunney")
+#     else:
+#         print("tea time")
+# else:
+#     print("its moons time")
+
+# time= float(input("what is the time now ? "))
+
+# if time == 8 or time <=10:
+#     print("It's breakfast time!")
+# elif time >= 13 and time<=15:
+#     print("It's lunch time!")
+# elif time >= 20 and time<=22:
+#     print("It's dinner time!")
+# else:
+#     print("It's not a meal time.")
+
+
+
+# day = "Saturday"
+# is_raining =True
+
+# if day == "Saturday" or day == "Sunday":
+#     if not is_raining:
+#         print("Let's visit Mysuru!")
+#     else:
+#         print("It's raining, let's stay home.")
+# else:
+#     print("It's a weekday, let's wait for the weekend.")
+
+#i=1
+# while i<=10:
+#     print("5*",i,"=",i*5)
+#     i=i+1
+
+
+
+# def greet():
+#     print("Dont eat anything")
+# #greet()
+
+# time= float(input("what is the time now ? "))
+
+# if time == 8 and time <=10:
+#     print("It's breakfast time!")
+# elif time >= 13 and time<=15:
+#     print("It's lunch time!")
+# elif time >= 20 and time<=22:
+#     print("It's dinner time!")
+# else:
+#     print("It's not a meal time.")
+#     greet()
+
+
+
+# sheep_count = 1
+# while sheep_count <= 10:
+#     print(f"Sheep {sheep_count}")
+#     sheep_count += 1
+
+# stu =1
+# while stu <=25:
+#     if stu <= 5:
+#         print("new entry - ",stu)
+#     elif stu ==10:
+#         stu+=1
+#         break
+#         stu+=1
+#         continue
+#     elif stu <15:
+#         print("3 rd time",stu)
+        
+#     stu+=1
+# print(" adm closed")
+
+# sheep_count = 1
+# while sheep_count <= 5:
+#     if sheep_count == 4:
+#         sheep_count += 1
+#         continue
+#     print(f"Sheep {sheep_count}")
+#     sheep_count += 1
+
+# pin=int(input("enter ypy pin - "))
+
+# pin=""
+# cor_pin=789
+# i=1
+# while pin and i==3:
+#     if pin ==int(input("enter ypy pin - ")):
+#         print("correct")
+#     else:
+#         print("incorrect pin")
+#         i=i+1
+    
+
+        
+    
+
+# pin = ""
+# correct_pin = "1234"
+# while pin != correct_pin:
+#     pin = input("Enter your PIN: ")
+#     if pin != correct_pin:
+#         print("Incorrect PIN. Try again.")
+# print("PIN accepted. You can proceed.")
+
+pin=123
+
+tril=1
+
+while tril<=3:
+    cor_pin=input(f"1st {tril}enter the pin -")
+    if pin==cor_pin:
+        print("log in success")
+        break
+    else:
+        print("incorrect pin number")
+
+
